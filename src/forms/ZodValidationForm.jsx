beb2bf03870f465702a29431edf6b88be55caa9a
Rinidh <rinidhdhokia@gmail.com>
2025-10-07 07:05:46 +0300

    Add grouped fields to ZodValidationForm with "personal" and "company" groups of inputs. Set the form to revalidate on every change in input and blur/focus Show error messgaes below each input field

--- src/forms/ZodValidationForm.jsx
+++ src/forms/ZodValidationForm.jsx
@@ -1,42 +1,107 @@
-import React from "react";
-import { useForm } from "react-hook-form";
-import * as z from "zod";
-import { zodResolver } from "@hookform/resolvers/zod";
+import React from 'react';
+import { useForm } from 'react-hook-form';
+import * as z from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
 
 const schema = z.object({
-  name: z.string().trim().min(2, "Name must be at least 2 characters"),
-  age: z.number().gte(18, "Must be 18 or older").optional(),
-  employeeId: z.string().regex(/^VVS-/, "Employee ID must start with VVS-"),
+  personal: z.object({
+    name: z.string().trim().min(2, 'Name must be at least 2 characters'),
+    age: z
+      .number('Please enter a valid age number')
+      .gte(18, 'Must be 18 or older')
+      .optional(),
+  }),
+  company: z.object({
+    employeeId: z.string().regex(/^VVS-/, 'Employee ID must start with VVS-'),
+    position: z.enum(
+      ['Director', 'Manager', 'Worker'],
+      'Position is either Director, Manager or Worker'
+    ),
+  }),
 });
 
 export const ZodValidationForm = () => {
-  const { register, handleSubmit } = useForm({
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
     resolver: zodResolver(schema),
+    mode: 'onChange', // revalidtes on every input
+    reValidateMode: 'onBlur', // revalidates also upon input losing focus
   });
 
   return (
     <form onSubmit={handleSubmit((d) => console.log(d))}>
+      <pre>{JSON.stringify(errors, null, 2)}</pre>
       <fieldset>
         <legend>Personal details</legend>
         <label>
           Name:
-          <input type="text" {...register("name")} />
+          <input type='text' {...register('personal.name')} />
         </label>
+        {errors.personal?.name && (
+          <p role='alert'>{errors.personal?.name.message}</p>
+        )}
+
         <label>
           Age:
-          <input type="text" {...register("age")} />
+          <input
+            type='text'
+            {...register('personal.age', {
+              valueAsNumber: true,
+              setValueAs: (age) => Math.round(age),
+            })}
+          />
         </label>
+        {errors.personal?.age && (
+          <p role='alert'>{errors.personal?.age.message}</p>
+        )}
       </fieldset>
 
       <fieldset>
         <legend>Company related</legend>
         <label>
           Employee ID:
-          <input type="text" {...register("employeeId")} />
+          <input type='text' {...register('company.employeeId')} />
         </label>
+        {errors.company?.employeeId && (
+          <p role='alert'>{errors.company?.employeeId.message}</p>
+        )}
+
+        <div>
+          Position:
+          <label>
+            Director
+            <input
+              type='radio'
+              {...register('company.position')}
+              value={'Director'}
+            />
+          </label>
+          <label>
+            Manager
+            <input
+              type='radio'
+              {...register('company.position')}
+              value={'Manager'}
+            />
+          </label>
+          <label>
+            Worker
+            <input
+              type='radio'
+              {...register('company.position')}
+              value={'Worker'}
+            />
+          </label>
+        </div>
+        {errors.company?.position && (
+          <p role='alert'>{errors.company?.position.message}</p>
+        )}
       </fieldset>
 
-      <input type="submit" />
+      <input type='submit' />
     </form>
   );
 };
