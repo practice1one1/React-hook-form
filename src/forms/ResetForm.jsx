dfd25ee20ed66a8df202d0682923abb33322fef3
Rinidh <rinidhdhokia@gmail.com>
2025-10-12 21:49:41 +0300

    Practice resetting a particular field only using `resetField()` Demonstrate 3 different resets: 1. Reset all including field values and touched/dirty state 2. Reset field values / user inputs but keep touched/dirty boolean state 3, Reset touched/dirty boolean state but keep the field values (user inputs)

--- src/forms/ResetForm.jsx
+++ src/forms/ResetForm.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState, useCallback } from 'react';
 import { useForm } from 'react-hook-form';
-import { getResetDataFromAPI } from '../../mocks/getResetDataFromAPI';
 
 export const ResetForm = () => {
   const {
@@ -8,13 +7,21 @@ export const ResetForm = () => {
     handleSubmit,
     formState: { errors, touchedFields, dirtyFields },
     watch,
-    reset,
-  } = useForm();
-
-  const resetFormAsync = useCallback(async () => {
-    const valuesToResetWith = await getResetDataFromAPI();
-    reset(valuesToResetWith);
+    resetField,
+    setValue,
+    getValues,
+  } = useForm({
+    defaultValues: {
+      field1: 'default val field1',
+      field2: 'default val field2',
+      field3: 'default val field3',
+    },
   });
+
+  // console.log('Field values:', watch());
+  console.log('%cTouched fields', 'color: lightblue', touchedFields);
+  // console.log('%cDirty fields', 'color: yellow', dirtyFields);
+  // console.log('%cError fields', 'color: red', errors);
 
   return (
     <>
@@ -24,17 +31,62 @@ export const ResetForm = () => {
           (err) => console.error('Submit error:', err)
         )}
       >
-        <input type='text' {...register('field1')} />
-        <input type='text' {...register('field2')} />
+        <input
+          type='text'
+          {...register('field1', {
+            minLength: {
+              value: 2,
+              message: 'Please enter at least 2 characters',
+            },
+          })}
+        />
+        <button type='button' onClick={() => resetField('field1')}>
+          Reset both touched/dirty state & field values (user input) state of
+          Field 1
+        </button>
 
+        <input
+          type='text'
+          {...register('field2', {
+            minLength: {
+              value: 2,
+              message: 'Please enter at least 2 characters',
+            },
+          })}
+        />
         <button
           type='button'
-          onClick={() =>
-            // can also call resetFormAsync() insided a useEffect(). Make sure the useEffect()s inside useForm() finish running before you reset the form. Here, the user is guaranteed to click this button (to reset the form) after useEffect()s have been called (happens quickly on render)
-            resetFormAsync()
-          }
+          onClick={() => {
+            resetField('field2', {
+              // keepDirty: true,
+              // keepError: true,
+              keepTouched: true,
+            });
+          }}
         >
-          Reset...
+          Reset field values (user input) state but retain touched/dirty state
+          in Field 2
+        </button>
+
+        <input
+          type='text'
+          {...register('field3', {
+            minLength: {
+              value: 2,
+              message: 'Please enter at least 2 characters',
+            },
+          })}
+        />
+        <button
+          type='button'
+          onClick={() => {
+            const currentField2Value = getValues('field3');
+            resetField('field3'); // resets all, including touched state & field values
+            setValue('field3', currentField2Value); // applies back the field values
+          }}
+        >
+          Reset touched/dirty state but retain field values (user input) in
+          Field 3
         </button>
 
         <input type='submit' />
