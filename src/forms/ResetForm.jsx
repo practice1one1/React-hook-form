fef0490671ef9755a1360a0ac71d0a7c7cb7f2ab
Rinidh <rinidhdhokia@gmail.com>
2025-10-13 07:00:58 +0300

    Practice resetting a form only for its error fields for user to re-attempt filling in new info.

--- src/forms/ResetForm.jsx
+++ src/forms/ResetForm.jsx
@@ -1,27 +1,34 @@
-import React, { useEffect, useState, useCallback } from 'react';
+import React, { useCallback } from 'react';
 import { useForm } from 'react-hook-form';
 
 export const ResetForm = () => {
   const {
     register,
     handleSubmit,
-    formState: { errors, touchedFields, dirtyFields },
-    watch,
+    formState: { errors, isValid, isDirty, touchedFields, dirtyFields },
     resetField,
-    setValue,
-    getValues,
+    watch,
   } = useForm({
     defaultValues: {
-      field1: 'default val field1',
-      field2: 'default val field2',
-      field3: 'default val field3',
+      name: '',
+      email: '',
+      address: 'Plot 1, Garden road',
     },
   });
 
-  // console.log('Field values:', watch());
+  console.log('Field values:', watch());
   console.log('%cTouched fields', 'color: lightblue', touchedFields);
-  // console.log('%cDirty fields', 'color: yellow', dirtyFields);
-  // console.log('%cError fields', 'color: red', errors);
+  console.log('%cDirty fields', 'color: yellow', dirtyFields);
+  console.log('%cError fields', 'color: red', errors);
+
+  const handleResetErrorFields = useCallback(() => {
+    Object.keys(errors).forEach((errorField) => {
+      resetField(errorField, {
+        keepDirty: true,
+        keepTouched: true,
+      });
+    });
+  });
 
   return (
     <>
@@ -33,62 +40,43 @@ export const ResetForm = () => {
       >
         <input
           type='text'
-          {...register('field1', {
+          placeholder='Name'
+          {...register('name', {
+            required: 'Name is required',
             minLength: {
               value: 2,
               message: 'Please enter at least 2 characters',
             },
           })}
         />
-        <button type='button' onClick={() => resetField('field1')}>
-          Reset both touched/dirty state & field values (user input) state of
-          Field 1
-        </button>
+        {errors.name && <p role='alert'>{errors.name.message}</p>}
 
         <input
           type='text'
-          {...register('field2', {
-            minLength: {
-              value: 2,
-              message: 'Please enter at least 2 characters',
-            },
+          placeholder='Email'
+          {...register('email', {
+            pattern: { value: /@/, message: 'Please enter a valid email' },
           })}
         />
-        <button
-          type='button'
-          onClick={() => {
-            resetField('field2', {
-              // keepDirty: true,
-              // keepError: true,
-              keepTouched: true,
-            });
-          }}
-        >
-          Reset field values (user input) state but retain touched/dirty state
-          in Field 2
-        </button>
+        {errors.email && <p role='alert'>{errors.email.message}</p>}
 
         <input
           type='text'
-          {...register('field3', {
+          placeholder='Address'
+          {...register('address', {
             minLength: {
               value: 2,
-              message: 'Please enter at least 2 characters',
+              message: 'Please enter a descriptive address',
             },
           })}
         />
-        <button
-          type='button'
-          onClick={() => {
-            const currentField2Value = getValues('field3');
-            resetField('field3'); // resets all, including touched state & field values
-            setValue('field3', currentField2Value); // applies back the field values
-          }}
-        >
-          Reset touched/dirty state but retain field values (user input) in
-          Field 3
-        </button>
+        {errors.address && <p role='alert'>{errors.address.message}</p>}
 
+        {!isValid && isDirty && (
+          <button type='button' onClick={handleResetErrorFields}>
+            Reset error fields only
+          </button>
+        )}
         <input type='submit' />
       </form>
     </>
